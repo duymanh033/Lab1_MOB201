84c6183e6eb66ed692fa5147a4d4165f3e3688b1
DUY MANH <duytqph20012@fpt.edu.vn>
2022-11-11 15:13:40 +0700

    init duy

--- app/src/main/java/com/example/lab1_mob201/duyMainActivity2.java
+++ app/src/main/java/com/example/lab1_mob201/duyMainActivity2.java
@@ -3,8 +3,10 @@ package com.example.lab1_mob201;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class duyMainActivity2 extends AppCompatActivity {
+    TextView a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
