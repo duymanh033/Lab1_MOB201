2deb687a10926273a3392054df7d9f6d65fbb702
TungLamzxje <105185027+TungLamzxje@users.noreply.github.com>
2022-11-11 14:40:28 +0700

    init test tạo layout mới

---
+++ app/src/main/java/com/example/lab1_mob201/TungLamLayout1.java
@@ -0,0 +1,14 @@
+package com.example.lab1_mob201;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TungLamLayout1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tung_lam_layout1);
+    }
+}
