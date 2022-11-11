1f9d2cc13d2ae0829076673ed5c4b8472a686f05
DUY MANH <duytqph20012@fpt.edu.vn>
2022-11-11 15:04:42 +0700

    init duy

---
+++ app/src/main/java/com/example/lab1_mob201/duyMainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.lab1_mob201;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class duyMainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_duy_main2);
+    }
+}
